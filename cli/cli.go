86497305fb0febe2ef5964022392156641ae61f5
Hussein Elguindi <34726859+HusseinElguindi@users.noreply.github.com>
2021-03-15 19:28:26 -0400

    removed some debug code

--- cli/cli.go
+++ cli/cli.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"os"
+	"runtime"
 
 	"github.com/husseinelguindi/mtd"
 )
 
 func main() {
+	// debug url
 	URL := "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1920_18MG.mp4"
 
 	f, err := os.OpenFile("./vid.mp4", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.FileMode(0666))
@@ -16,7 +18,8 @@ func main() {
 	}
 	defer f.Close()
 
-	task := mtd.New(URL, 1, 1024*1024*17, f)
+	chunks := runtime.NumCPU()
+	task := mtd.New(URL, uint32(chunks), 1024*64, f) // 64kb read/write size
 	if err := task.DownloadHTTP(); err != nil {
 		log.Fatalln(err)
 	}
